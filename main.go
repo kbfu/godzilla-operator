f7871f46c777946ee786774eef9a1a23aae24757
kbfu <d.zeta@163.com>
2023-12-13 23:31:55 +0800

    fix

--- main.go
+++ main.go
@@ -113,6 +113,13 @@ func main() {
 		logrus.Error(err, "unable to create controller", "controller", "GodzillaJob")
 		os.Exit(1)
 	}
+	if err = (&controllers.GodzillaJobSnapshotReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		logrus.Error(err, "unable to create controller", "controller", "GodzillaJobSnapshot")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
